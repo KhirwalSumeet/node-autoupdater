b41dfbc8eb06b9af395a2c7ae32c9a5bae9da766
KhirwalSumeet <sumeetkhirwal@gmail.com>
2019-03-11 14:50:00 -0400

    Auto deployment done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var execFile = require('child_process').execFile;
+const exec = require('child_process').exec;
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
@@ -11,18 +11,20 @@ const bodyParser = require('body-parser');
   app.use(cookieParser());
 
 app.post("/master", function (req, res) {
-  req = req.body;
-  payload = JSON.parse(req.payload);
-  console.log('Is this a master branch ? ' + payload.ref.includes('master'));
+    req = req.body;
+    payload = JSON.parse(req.payload);
+    console.log('Is this a master branch ? ' + payload.ref.includes('master'));
     if( payload.ref.includes('master') )
     {
-            // Exec a shell script
-            execFile('restart.sh', function(error, stdout, stderr) {
-                    // Log success in some manner
-                    console.log( 'exec complete' );
-            });
+        exec('cd /home/ubuntu/cricgod-ui && npm install && pm2 restart 0', (e, stdout, stderr)=> {
+			if (e instanceof Error) {
+				console.error(e);
+				throw e;
+			}
+			console.log('stdout ', stdout);
+		});
     }
-  res.send(200);
+    res.send(200);
 })
 
 app.listen(3001, function (err) { });
